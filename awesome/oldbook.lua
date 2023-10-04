d3536a2b688a557a83955728952293dbba4e6dc5
PoisonIsBestType <143094351+PoisonIsBestType@users.noreply.github.com>
2023-10-04 14:43:49 -0300

    Add files via upload

---
+++ awesome/oldbook.lua
@@ -0,0 +1,103 @@
+    -- OldBook colorscheme
+
+    -- A light colorscheme for dark theme users
+    -- Goes well with Solarized GTK themes and other  yellowish/tan themes
+    
+    return{
+    
+        -- Base
+        background = {
+            nrm = '#E9E2C9',
+            alt = '#E9E1C3',
+            fcs = '#DAC9AA'
+        },
+        foreground = {
+            nrm = '#585148',
+            alt = '#583A24'
+        },
+        accent = {
+            nrm = '#585148',   
+            alt = '#583A24', 
+            brt_nrm = '#7A1405', 
+            brt_alt = '#158f9C'
+        },
+    
+        -- Normal
+        normal = {
+            black = '#2E2928',
+            red = '#5E0f04',
+            green = '#333f0A',
+            yellow = '#957C16',
+            blue = '#2A5173',
+            magenta = '#513E56',
+            cyan = '#0E616A',
+            white = '#42474C',
+            orange = '#683504',
+            pink = '#5F2263',
+            brown = '#523622'
+        },
+    
+        -- Bright
+        bright = {
+            black = '#413D37',
+            red = '#7A1405',
+            green = '#50620F',
+            yellow = '#B4961A',
+            blue = '#366994',
+            magenta = '#674f6E',
+            cyan = '#158f9C',
+            white = '#413D37',
+            orange = '#984E06',
+            pink = '#85308A',
+            brown = '#784F31'
+        },
+    
+        -- Content grey
+    
+        content = {
+            c1 = '#262422',
+            c2 = '#3D3A37',
+            c3 = '#4F4B47',
+            c4 = '#66615C',
+            c5 = '#7D7770',
+            c6 = '#918A83'
+        },
+    
+    
+        -- Gradient 8
+        grad8 = {
+            g1 = '#7A1405',
+            g2 = '#6C261B',
+            g3 = '#5E3631',
+            g4 = '#504746',
+            g5 = '#435658',
+            g6 = '#386369',
+            g7 = '#2A747D',
+            g8 = '#158f9C'
+        },
+    
+        -- Gradient 16
+        grad16 = {
+            g1 = '#7A1405',
+            g2 = '#731D10',
+            g3 = '#6C261B',
+            g4 = '#652E26',
+            g5 = '#5E3631',
+            g6 = '#56403D',
+            g7 = '#504746',
+            g8 = '#4A4E4F',
+            g9 = '#435658',
+            g10 = '#3E5C60',
+            g11 = '#386369',
+            g12 = '#316B73',
+            g13 = '#2A747D',
+            g14 = '#237D87',
+            g15 = '#1C8691',
+            g16 = '#158f9C'
+        },
+            
+        black = '#000000',
+        white = '#FFFFFF'
+    
+    }
+    
